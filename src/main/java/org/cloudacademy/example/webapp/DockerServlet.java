29a239752b497c234f999ab95c88258d3a4e2b47
SamyGev <73714625+SamyGev@users.noreply.github.com>
2024-07-08 10:14:33 +0200

    Update DockerServlet.java

--- src/main/java/org/cloudacademy/example/webapp/DockerServlet.java
+++ src/main/java/org/cloudacademy/example/webapp/DockerServlet.java
@@ -92,18 +92,21 @@ public class DockerServlet extends HttpServlet {
         case "/action1":
             logger.debug("action 1 called...");
             response.getWriter().println("action->1");
+            response.getWriter().println(containerId);
+            response.getWriter().println(containerIp);
             break;
         case "/action2":
             logger.debug("action 2 called...");
             response.getWriter().println("action->2");
+            response.getWriter().println(containerId);
+            response.getWriter().println(containerIp);
             break;
         case "/action3":
             logger.debug("action 3 called...");
             response.getWriter().println("action->3");
-            
+            response.getWriter().println(containerId);
+            response.getWriter().println(containerIp);
             break;
         }
-        response.getWriter().println(containerId);
-        response.getWriter().println(containerIp);
     }
 }
